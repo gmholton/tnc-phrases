036c1d0ccd4ff8c4827c653b4a199aba964b2692
Gary Holton <gmholton@users.noreply.github.com>
2023-02-23 08:21:25 -1000

    cleanup

--- scripts/combine.py
+++ scripts/combine.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
-# append f2 to f1
+# This just joins to audio files together, in case
+# the segmenter incorrectly separated them
+# Run from command line with: secnum , track1, track2
+#
+# Warning! No error handling
+#
 import sys, os
 from pydub import AudioSegment
 
-s1 = 'tnc/tnc-' + str(sys.argv[1]).zfill(2) + "-" + str(sys.argv[2]).zfill(2) + ".wav"
-s2 = 'tnc/tnc-' + str(sys.argv[1]).zfill(2) + "-" + str(sys.argv[3]).zfill(2) + ".wav"
+indir = "tnc/"
+prefix = "tnc"
+sep = "-"
+soundformat = "wav"
 
-sound1 = AudioSegment.from_file(s1, format="wav")
-sound2 = AudioSegment.from_file(s2, format="wav")
+
+s1 = indir + prefix + sep + str(sys.argv[1]).zfill(2) + sep + str(sys.argv[2]).zfill(2) + "." + soundformat
+s2 = indir + prefix + sep + str(sys.argv[1]).zfill(2) + sep + str(sys.argv[3]).zfill(2) + "." + soundformat
+
+sound1 = AudioSegment.from_file(s1, format= soundformat )
+sound2 = AudioSegment.from_file(s2, format= soundformat)
 
 combined = sound1 + sound2
 
-combined.export(s1, format="wav")
-os.remove(s2)
+combined.export(s1, format=soundformat)
+os.remove(s2) # watch out! deleting sound2 
